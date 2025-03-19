22984898f001c1e209ddf8968289e363874ea2c1
Xinyu Wang <2863940W@student.gla.ac.uk>
2025-03-19 03:18:57 +0000

    Update settings and requirements for deployment

--- library_management/settings.py
+++ library_management/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c!s3f0mly-%jty^rfx1_2h+g7n!u#6=gpx-nfr^2ur&+jx-(ro')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
@@ -56,6 +56,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'library_management.wsgi.application'
 
+# 静态文件配置
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 媒体文件配置
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# 使用whitenoise处理静态文件
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # 数据库配置
 DATABASES = {
     'default': dj_database_url.config(
@@ -72,16 +84,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # 登录URL设置
 LOGIN_URL = 'login'
