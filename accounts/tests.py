b748acab92d3e32ec22ad9621f181864c0437ce5
Xinyu Wang <2863940W@student.gla.ac.uk>
2025-03-20 03:36:09 +0000

    recommit

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,6 @@
-
-from django.test import TestCase
-
 # Create your tests here.
-from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import reverse
+from django.test import TestCase
 
 User = get_user_model()
 
