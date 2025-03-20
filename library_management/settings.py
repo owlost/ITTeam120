bcb78eba6a245f4044d0d94532a1ec4731ddb437
Xinyu Wang <2863940W@student.gla.ac.uk>
2025-03-20 02:53:58 +0000

    recommit

--- library_management/settings.py
+++ library_management/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -62,12 +61,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'library_management.wsgi.application'
 
-# Database configuration for Heroku
+# 使用 SQLite 数据库
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 LANGUAGE_CODE = 'en-us'
